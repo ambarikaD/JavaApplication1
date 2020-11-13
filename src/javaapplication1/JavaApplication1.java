44318a9e0e7504c8dc8a04bf7838cb31a5adfa7d
ambarikaD <dasriambarika@gmail.com>
2020-11-13 18:08:28 +0700

    bkkjg

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,7 +16,8 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("cobaaaaaaa"()");
+        System.out.println("cobaaaaaaa"()");"
+        System.out.println("javaapplication1.JavaApplication1.main()");
     }
     
     
