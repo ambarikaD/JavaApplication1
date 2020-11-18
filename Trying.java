29afe47e46dd47ff2f37b3c69e35fe021c38679b
ambarikaD <74280460+ambarikaD@users.noreply.github.com>
2020-11-18 10:58:35 +0700

    Add files via upload

---
+++ Trying.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trying;
+
+/**
+ *
+ * @author win10
+ */
+public class Trying {
+
+  
+    public static void main(String[] args) {
+        double a= 100;
+        double b= 23;
+        
+        double tambah = a/b;
+        
+        float a1= 100;
+        float b1= 23;
+        
+        float bagi = a1/b1;
+        
+        String kata = "ini kata 901!!@#23";
+        char karakter = '8';
+        
+        int x=100;
+        int y=23;
+        
+        int integer = x/y;
+        
+        int p=100;
+        int q=20;
+        int modulusPQ = p%q;
+        int modulusXY = x%y;
+        
+        System.out.println("hasil bagi int = "+integer);
+        System.out.println("hasil bagi double = "+tambah);
+        System.out.println("hasil bagi modulus p%q = "+modulusPQ);
+        System.out.println("hasil bagi modulus x%y = "+modulusXY);
+        System.out.println("hasil bagi float = "+bagi);
+        System.out.println("output string = "+kata);
+        System.out.println("output char = "+karakter);
+    }
+}
+/**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
